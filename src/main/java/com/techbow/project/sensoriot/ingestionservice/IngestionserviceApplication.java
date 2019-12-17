6eaaa216cc0fea27d49e225aa1693bd83eb45772
Bo Yuan <boyuan@Bos-MacBook-Pro-2.local>
2019-12-16 23:22:18 -0800

    initiate

---
+++ src/main/java/com/techbow/project/sensoriot/ingestionservice/IngestionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.techbow.project.sensoriot.ingestionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestionserviceApplication.class, args);
+	}
+
+}
